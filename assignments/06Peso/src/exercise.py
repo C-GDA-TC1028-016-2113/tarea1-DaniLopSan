f62d8191849f6043ff546564c3071b88addb3367
DaniLopSan <A01641250@itesm.mx>
2021-08-20 00:48:18 +0000

    Intento1

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    pesoInicial = float(input("Dame el peso inicial: "))
+    pesoFinal = float(input("Dame el peso final: "))
+    meses = float(input("Dame la cantidad de meses: "))
+    porMes = (pesoInicial - pesoFinal) / meses
+    print("Lo que debes bajar por mes es:", porMes)
+
+
+
 
 
 
